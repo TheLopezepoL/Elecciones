20bdaa3f68fba268cef342dd8d0d5b7685772f60
TheLopezepoL <thelopezepol@outlook.es>
2019-05-27 01:36:19 -0600

    Estructura PP&PS

---
+++ elecciones.py
@@ -0,0 +1,24 @@
+###############################
+# Creadores: Sebastián López y Daniel Sequeira
+# Creado el: 26/05/19 22:30
+# Ult. Actualización: 26/05/19 23:26
+# Version 0.1.0 Python 3.7.3
+###############################
+# Importación de Librerias
+from funcionesSLH import *
+
+# Definición de Funciones
+
+
+# Creación de GUI
+
+
+# Programa Principal
+
+
+# - FIN - #
+#####  #####  #####  #   #   ##    #######
+#        #    #      #   #   # #  #       #
+###      #    #####  #####   #  ##         #
+#        #        #  #   #   # #  #       #
+#      #####  #####  #   #   ##    #######
---
+++ funcionesSLH.py
@@ -0,0 +1,18 @@
+###############################
+# Creadores: Sebastián López y Daniel Sequeira
+# Creado el: 26/05/19 22:30
+# Ult. Actualización: 26/05/19 23:26
+# Version 0.1.0 Python 3.7.3
+###############################
+# Importación de Librerias
+
+
+# Definición de Funciones
+
+
+# - FIN - #
+#####  #####  #####  #   #   ##    #######
+#        #    #      #   #   # #  #       #
+###      #    #####  #####   #  ##         #
+#        #        #  #   #   # #  #       #
+#      #####  #####  #   #   ##    #######
