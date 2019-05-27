26b8cb628eab20b7fd3ce3aea5e9e1bc32ca0d6c
TheLopezepoL <thelopezepol@outlook.es>
2019-05-27 01:40:58 -0600

    PP&PS

--- elecciones.py
+++ elecciones.py
@@ -1,8 +1,8 @@
 ###############################
 # Creadores: Sebastián López y Daniel Sequeira
 # Creado el: 26/05/19 22:30
-# Ult. Actualización: 26/05/19 23:26
-# Version 0.1.0 Python 3.7.3
+# Ult. Actualización: 27/05/19 01:40
+# Version 0.1.1 Python 3.7.3
 ###############################
 # Importación de Librerias
 from funcionesSLH import *
